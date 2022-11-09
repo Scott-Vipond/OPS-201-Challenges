b0a83da2a716e5e1ea74027cef37088dd50e6218
Scott-Vipond <scott.donna.vipond@outlook.com>
2022-11-09 23:05:45 +0000

    Changed Function to Login History and Text

---
+++ ops-201-challenge03.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script: Functions                      
+# Author: Scott Vipond                      
+# Date of latest revision: 9 Nov 22
+# Purpose: OPS-201 Challenge03 - Functions
+
+# Basic function
+print_something () {
+  echo "Hello I am a function that displays the login history"
+}
+var="Server Login History"
+echo $var
+last
+print_something
